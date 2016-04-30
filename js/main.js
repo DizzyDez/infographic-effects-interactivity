628936523507b7e5b7e1ba00a10406c6c402274a
DizzyDez <desc0137@algonquinlive.com>
2016-04-29 22:53:00 -0400

    working great for testing

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $million.waypoint(function (direction) {
   if (direction == 'down') {
     $million.addClass('js-animate');
   } else {
-  $million.removeClass('js-animate');
+    $million.removeClass('js-animate');
   }
 }, {offset: '50%'});
 
@@ -24,30 +24,30 @@ $sevenfive.waypoint(function (direction) {
   if (direction == 'down') {
     $sevenfive.addClass('js-animate');
   } else {
-  $sevenfive.removeClass('js-animate');
+    $sevenfive.removeClass('js-animate');
   }
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $piechart.waypoint(function (direction) {
   if (direction == 'down') {
     $piechart.addClass('js-animate');
   } else {
-  $piechart.removeClass('js-animate');
+    $piechart.removeClass('js-animate');
   }
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $bubble.waypoint(function (direction) {
   if (direction == 'down') {
     $bubble.addClass('js-animate');
   } else {
-  $bubble.removeClass('js-animate');
+    $bubble.removeClass('js-animate');
   }
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $sportbysport.waypoint(function (direction) {
   if (direction == 'down') {
     $sportbysport.addClass('js-animate');
   } else {
-  $sportbysport.removeClass('js-animate');
+    $sportbysport.removeClass('js-animate');
   }
-}, { offset: '50%'});
+}, {offset: '50%'});
